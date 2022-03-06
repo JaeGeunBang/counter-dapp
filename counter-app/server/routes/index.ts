aaaaf91f32178ec382ed5a3978bdfbbdb88e43e9
addy.b <addy.b@kakaoenterprise.com>
2022-03-06 16:10:57 +0900

    counter-contract 수정

--- counter-app/server/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import express, { Request, Response } from 'express';
-
-export const indexRouter = express.Router() ;
-
-indexRouter.get('/', async (req: Request, res: Response) => {
-    res.status(200).send(req.cookies)
-});
